17ff4c3d635d156cf5387261b19c8b5763907032
derek <derek@wigger.gay>
2023-02-16 08:09:17 -0500

    Add more datetime constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 import { ApplicationCommandOptionChoiceData } from 'discord.js';
 import moment from 'moment-timezone';
 
-const weekdays: ApplicationCommandOptionChoiceData[] = [
+const weekdays: ApplicationCommandOptionChoiceData<number>[] = [
     {
         name: 'Sunday',
         value: 1,
@@ -32,22 +32,31 @@ const weekdays: ApplicationCommandOptionChoiceData[] = [
     },
 ];
 
-// Generate times list in 15 minute blocks
-const timeslist: ApplicationCommandOptionChoiceData<string>[] = [];
-for (let i = 0; i < 24; i++) {
-    for (let j = 0; j < 4; j++) {
-        timeslist.push(
-            {
-                name: `${i}:${j === 0 ? `00` : 15 * j}`,
-                value: `${i}:${j === 0 ? `00` : 15 * j}`
-            }
-        );
-    }
-}
-
 // Generate array of momentjs timezone strings for the US
 // (this is what gets stored in the model as User.timezone)
 const timezones = moment.tz.names().filter(x => x.startsWith('US/'));
 
+// Generate months from moment.js
+const months = moment.months().map((x, i) => {return {name: x, value: i+1}});
 
-export { weekdays, timeslist, timezones};
+/**
+ * Generates an array of date strings as command option objects 
+ * @param days The number of days into the future to generate the list
+ */
+const datesOptions = (days: number = 24) => {
+    const today = new Date();
+    const datesOptions: ApplicationCommandOptionChoiceData<string>[] = [];
+    for (let i=0;i<=24; i++){
+        const nextDay = new Date(today.setDate(1 + today.getDate()));
+        datesOptions.push(
+            {
+                name: nextDay.toLocaleDateString('en-US'),
+                value: nextDay.toLocaleDateString('en-US')
+            }
+        )
+    }
+    return datesOptions;
+}
+
+
+export { weekdays, timezones, months, datesOptions };
