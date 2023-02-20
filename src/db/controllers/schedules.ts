d1ad2cafea9a0fcb5b0aff1bf1bec80a0a05515e
derek <derek@wigger.gay>
2023-02-19 21:53:24 -0500

    Add CreateScheduleWindow controller

--- src/db/controllers/schedules.ts
+++ src/db/controllers/schedules.ts
@@ -1,6 +1,6 @@
 import { Op } from "sequelize";
 import db from "../connect";
-import { AvailabilityRecurringAttributes, initModels } from "../models/init-models";
+import { AvailabilityRecurringAttributes, AvailabilityWindow, AvailabilityWindowAttributes, initModels } from "../models/init-models";
 const { AvailabilityRecurring, Frequency } = initModels(db);
 
 /**
@@ -23,6 +23,15 @@ async function InsertRecurringSchedule(entity: AvailabilityRecurringAttributes) 
     return AvailabilityRecurring.create(entity);
 }
 
+async function CreateScheduleWindow(entity: AvailabilityWindowAttributes) {
+    if (entity.is_available) {
+        return AvailabilityWindow.create(entity)
+    }
+    // else we need to update or delete the window of time that they can no longer make if it exists
+    // or we need to insert an is_available=false record if there's conflicts with the recurring schedule...
+    // we need an engine for this
+}
+
 /**
  * Remove a schedule for a user in the database
  * @param id The id of the recurring schedule in the database
@@ -33,4 +42,4 @@ async function DeleteRecurringSchedule(id: number) {
     });
 }
 
-export { GetRecurringSchedules, InsertRecurringSchedule, DeleteRecurringSchedule }
+export { GetRecurringSchedules, InsertRecurringSchedule, DeleteRecurringSchedule, CreateScheduleWindow }
