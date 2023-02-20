f78b38ab0eb2cfdad847a09d5eb0187741ce51fb
derek <derek@wigger.gay>
2023-02-19 21:52:13 -0500

    Add functionality for user insert and update

--- src/db/controllers/users.ts
+++ src/db/controllers/users.ts
@@ -1,6 +1,6 @@
 import { Op } from "sequelize";
 import db from "../connect";
-import { initModels } from "../models/init-models";
+import { initModels, UserAttributes } from "../models/init-models";
 const { User } = initModels(db);
 
 /**
@@ -8,9 +8,22 @@ const { User } = initModels(db);
  * @param discordId
  */
 async function GetUser (discordId: string) {
-    return await User.findOne({
+    return User.findOne({
         where: { discordId: { [Op.eq]: discordId } },
     });
 }
 
-export {GetUser}
+async function InsertUser (entity: UserAttributes) {
+    return User.create(entity);
+}
+
+async function UpsertUser (entity: UserAttributes) {
+    const {discordId, timezone} = entity;
+    let _user = await GetUser(discordId);
+    if (!_user || _user === undefined){
+        return await InsertUser({discordId, timezone});
+    }
+    return await User.update( {timezone}, { where: { discordId: { [Op.eq]: discordId} }});
+}
+
+export {GetUser, InsertUser, UpsertUser}
